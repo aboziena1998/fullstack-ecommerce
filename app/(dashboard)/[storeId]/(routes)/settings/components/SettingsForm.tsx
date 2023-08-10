bf38032fc1535bbd84d2605b893eb7d6a498a30f
abozienacoding <87572781+aboziena1998@users.noreply.github.com>
2023-08-10 19:11:53 +0300

    deleting store finished

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -20,6 +20,7 @@ import { Input } from '@/components/ui/input';
 import { toast } from 'react-hot-toast';
 import axios from 'axios';
 import { useParams, useRouter } from 'next/navigation';
+import { AlertModal } from '@/components/modals/alert-modal';
 
 interface SettingsFormProps {
   initialData: Store;
@@ -52,9 +53,29 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
       setLoading(false);
     }
   };
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      const res = axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push('/');
+      toast.success('Store Deleted');
+    } catch (error) {
+      toast.error('Make sure you removed all products and categories first');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferances" />
         <Button
